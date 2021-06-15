4706fe0916621df0f8245db3586d522b4bff3177
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-15 09:30:01 -0400

    add : adding welcome statement for main branch : BinaryTree.java

---
+++ BinaryTree.java
@@ -0,0 +1,22 @@
+/*******************************************************************************************************************************
+ * @purpose Implementing Binary Search Tree Program, This program shows , how the Binary Search tree method works
+ * Here we will be assigning one int base root key , based on that root key the next key will be added or inserted
+ * If the new key is lesser then base key it will be adding to left side of base key else it will be added to right side of base key.
+ * @author rekha
+ * @version 1.0
+ * @since 14-062021
+ *************************************************************************************************************************************/
+
+package com;
+
+/**
+ * @author rekha BinaryTree is an class it is in public AS
+ * @param <K> k= generic initialized root has private
+ */
+public class BinaryTree<K extends Comparable<K>> {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Binary Search Tree Program");
+	}
+}
